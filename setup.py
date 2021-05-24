a763e1aa18f9f02a2818508e669ed024f6907a62
Nocz <devnocz@gmail.com>
2021-05-24 16:13:57 -0500

    0.0.1 empty

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(
+    name="arg_checker",
+    version="0.0.1",
+    description="argument checker",
+    py_modules="check",
+    package_dir={"": "src"}
+
+)
