630a0328390c92443770d9004ce13ac7f931c18f
Nocz <devnocz@gmail.com>
2021-06-02 21:17:20 -0500

    setup

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
 
 setup(
-    name="arg_checker",
-    version="0.0.1",
-    description="argument checker",
-    py_modules="check",
-    package_dir={"": "src"},
-    python_requires=">=3.7.0"
+    name = 'generator-data-checker',
+    version = '1.0.0',
+    description = 'Python data checker withing a range',
+    url = 'https://github.com/Noczio/Python-Data-Chercker.git', 
+    author = 'Dev Nocz', 
+    author_email = 'devnocz@gmail.com', 
+    keywords = 'Python, data, check, generator', 
+    package_dir = {'': 'src'},
+    packages = find_packages(where='src'), 
+    python_requires='>=3.7',
 )
